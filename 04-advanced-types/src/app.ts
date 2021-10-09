5480c1bb55939214a48516cf4cbf4947dc2995a3
Matija Osrečki <matija.osrecki@gmail.com>
2021-10-09 16:51:57 +0200

    Index properties

--- 04-advanced-types/src/app.ts
+++ 04-advanced-types/src/app.ts
@@ -7,6 +7,7 @@ interface Employee {
   startDate: Date;
 }
 
+//* Intersection types
 // interface ElevatedEmployee extends Admin, Employee {}
 type ElevatedEmployee = Employee & Admin;
 
@@ -20,6 +21,7 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric; // type is number as thats the only intersection
 
+//* Type guards
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString();
@@ -75,7 +77,7 @@ function useVehicle(vehicle: Vehicle) {
 // useVehicle(v1);
 // useVehicle(v2);
 
-// Discriminated union - we have a common property that could be used to check
+//* Discriminated union - we have a common property that could be used to check
 interface Bird {
   type: 'bird';
   flyingSpeed: number;
@@ -102,6 +104,7 @@ function moveAnimal(animal: Animal) {
 // moveAnimal({ type: 'bird', flyingSpeed: 23 });
 // moveAnimal({ type: 'horse', runningSpeed: 45 });
 
+//* Type casting
 // const userInputElement2 = document.querySelector('input')!; // typescript knows its an input
 // const userInputElement = <HTMLInputElement>(document.getElementById('user-input'));
 const userInputElement = document.getElementById(
@@ -111,3 +114,14 @@ const userInputElement = document.getElementById(
 if (userInputElement) {
   userInputElement.value = 'Hi there';
 }
+
+//* Index properties
+interface ErrorContainer {
+  // we dont know how much props, or the name of props, but they all will be string:string
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'not a valid email',
+  username: 'name must be longer than 3 characters',
+};
