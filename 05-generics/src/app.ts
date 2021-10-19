51265aac2780446b0bda468a45ed4d0e206e00cb
Matija Osrečki <matija.osrecki@gmail.com>
2021-10-19 20:52:28 +0200

    Introduce custom generics type

--- 05-generics/src/app.ts
+++ 05-generics/src/app.ts
@@ -1,13 +1,24 @@
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-names[0].split(' ');
+// names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done');
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done');
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(' ');
-});
+// promise.then((data) => {
+//   data.split(' ');
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// const mergedObj = merge({ name: 'tomo' }, { age: 20 }) as {
+//   name: string;
+//   coder: boolean;
+// };
+const mergedObj = merge({ name: 'tomo' }, { age: 20 });
+console.log(mergedObj.age);
