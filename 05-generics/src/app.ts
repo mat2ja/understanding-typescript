4eeea49a38232bd6612e147b32a4d1a7ec80d2ac
Matija Osrečki <matija.osrecki@gmail.com>
2021-10-19 21:07:34 +0200

    Custom generics constraints

--- 05-generics/src/app.ts
+++ 05-generics/src/app.ts
@@ -18,7 +18,24 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge(
   { name: 'tomo', languages: ['flutter', 'golang'], hasBtc: false },
-  69
+  { age: 20 }
 );
+// console.log(mergedObj.name);
 
-console.log(mergedObj.name);
+interface Lengthy {
+  length: number;
+}
+
+function countAndPrint<T extends Lengthy>(element: T): [T, string] {
+  let description = 'Got no value';
+  if (element.length === 1) {
+    description = 'Got 1 element';
+  } else if (element.length > 1) {
+    description = `Got ${element.length} elements`;
+  }
+  return [element, description];
+}
+
+console.log(countAndPrint('hi there boomguzzler'));
+console.log(countAndPrint(['vue', 'svelte']));
+console.log(countAndPrint([]));
