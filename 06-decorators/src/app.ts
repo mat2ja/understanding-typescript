ca2c805865faf342d0811ee66bd6af3949856ed9
Matija Osrečki <matija.osrecki@gmail.com>
2022-01-04 11:30:21 +0100

    Advanced validation with decorators

--- 06-decorators/src/app.ts
+++ 06-decorators/src/app.ts
@@ -103,6 +103,8 @@ class Product {
   }
 }
 
+// ---------------------------------------------------
+
 const p1 = new Product('ledger nano x', 140);
 const p2 = new Product('ledger nano s +', 90);
 
@@ -136,3 +138,106 @@ const printer = new Printer();
 const button = document.querySelector('button')!;
 // button.addEventListener('click', printer.showMessage.bind(printer));
 button.addEventListener('click', printer.showMessage);
+
+// ---------------------------------------------------
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  const className = target.constructor.name;
+
+  registeredValidators[className] = {
+    ...registeredValidators[className],
+    [propName]: ['required'],
+  };
+  // { Course: { title: ['required'] }}
+}
+
+function PositiveNumber(target: any, propName: string) {
+  const className = target.constructor.name;
+
+  registeredValidators[className] = {
+    ...registeredValidators[className],
+    [propName]: Array.isArray(registeredValidators[className][propName])
+      ? [...registeredValidators[className][propName], 'positive']
+      : ['positive'],
+  };
+  // { Course: { price: ['positive'] }}
+}
+
+function TooLargeNumber(target: any, propName: string) {
+  const className = target.constructor.name;
+
+  registeredValidators[className] = {
+    ...registeredValidators[className],
+    [propName]: Array.isArray(registeredValidators[className][propName])
+      ? [...registeredValidators[className][propName], 'tooLarge']
+      : ['tooLarge'],
+  };
+  // { Course: { price: ['positive'] }}
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  console.log(objValidatorConfig);
+  if (!objValidatorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+        case 'tooLarge':
+          isValid = isValid && obj[prop] < 100;
+          break;
+        default:
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @TooLargeNumber
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form') as HTMLFormElement;
+courseForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const titleEl = document.querySelector('#title') as HTMLInputElement;
+  const priceEl = document.querySelector('#price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    return alert('Invalid input');
+  }
+
+  courseForm.reset();
+  console.log(createdCourse);
+});
